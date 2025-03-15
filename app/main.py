eff2c8dd1c7c38fda14d4a0d10e92546bb27e8e9
Jose Reynoso <reynosojose2005@gmail.com>
2025-03-14 18:31:42 -0600

    comment: vie 14 mar 2025 18:31:42 CST

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+import logging
+import json
+from datetime import datetime, timezone
+from prometheus_fastapi_instrumentator import Instrumentator
+
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("devops")
+
+def log_json(level, component, message, status_code=None):
+    log_entry = {
+        "level": level,
+        "time": datetime.now(timezone.utc).isoformat(),
+        "component": component,
+        "message": message
+    }
+    if status_code is not None:
+        log_entry["status_code"] = status_code
+    print(json.dumps(log_entry))
+
+app = FastAPI()
+
+# Instrumentar Prometheus
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/")
+async def always_ok():
+    log_json("info", "devops", "Service full operated.", 200)
+    return {"message": "Hello from devops service"}
+
+@app.get("/health")
+async def health_check():
+    log_json("info", "devops", "Health check endpoint called.", 200)
+    return {"status": "ok", "service": "devops"}
